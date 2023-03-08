6dccfee6506940f97019d3b0da172ccc694f5916
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-08 15:38:12 +0100

    commit

--- connectes.py
+++ connectes.py
@@ -5,7 +5,7 @@ sort and display.
 """
 
 from timeit import timeit
-from sys import argv
+from sys import argv, setrecursionlimit
 
 from geo.point import Point
 
@@ -23,11 +23,55 @@ def load_instance(filename):
     return distance, points
 
 
+def near(point, other, distance):
+    """
+    renvoie True si les points sont proches en abscisse
+    """
+    x_1 = point.coordinates[0]
+    x_2 = other.coordinates[0]
+    return abs(x_2 - x_1) <= distance
+
+
 def print_components_sizes(distance, points):
     """
     affichage des tailles triees de chaque composante
     """
-    pass  # TODO: afficher la solution
+    n_points = len(points)
+    points.sort()
+    processed = [False] * n_points
+    components_sizes = []
+    setrecursionlimit(1000000)
+
+    def component_size(i, j, j_min):
+        """
+        incremente la taille de la composante si les points sont proches
+        """
+        if j_min < j < n_points:
+            if not processed[j]:
+                point, other = points[i], points[j]
+                if near(point, other, distance):
+                    if point.distance_to(other) <= distance:
+                        processed[j] = True
+                        components_sizes[-1] += 1
+                        component_size(j, j+1, j_min)
+                        component_size(j, j-1, j_min)
+                    if i < j:
+                        component_size(i, j+1, j_min)
+                    else:
+                        component_size(i, j-1, j_min)
+            else:
+                if i < j:
+                    component_size(i, j+1, j_min)
+                else:
+                    component_size(i, j-1, j_min)
+
+    for i in range(n_points):
+        if not processed[i]:
+            processed[i] = True
+            components_sizes.append(1)
+            component_size(i, i+1, i)
+    components_sizes.sort(reverse=True)
+    print(components_sizes)
 
 
 def main():
