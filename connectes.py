5abbcc35603dd88c625dcb514f88d311174d2e1d
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-11 06:33:57 +0100

    commit

--- connectes.py
+++ connectes.py
@@ -5,7 +5,7 @@ sort and display.
 """
 
 from timeit import timeit
-from sys import argv, setrecursionlimit
+from sys import argv
 
 from geo.point import Point
 
@@ -38,38 +38,40 @@ def print_components_sizes(distance, points):
     """
     n_points = len(points)
     points.sort()
-    processed = [False] * n_points
     components_sizes = []
-    setrecursionlimit(1000000)
+    processed = set()
+    stack = []
 
-    def component_size(i, j, j_min):
-        """
-        incremente la taille de la composante si les points sont proches
-        """
-        if j_min < j < n_points:
-            if not processed[j]:
-                point, other = points[i], points[j]
-                if near(point, other, distance):
-                    if point.distance_to(other) <= distance:
-                        processed[j] = True
-                        components_sizes[-1] += 1
-                        component_size(j, j+1, j_min)
-                        component_size(j, j-1, j_min)
-                    if i < j:
-                        component_size(i, j+1, j_min)
-                    else:
-                        component_size(i, j-1, j_min)
-            else:
-                if i < j:
-                    component_size(i, j+1, j_min)
-                else:
-                    component_size(i, j-1, j_min)
+    def neighbors(i, j_min):
+        point = points[i]
+        point_x = point.coordinates[0]
+
+        j = i + 1
+        while j < n_points and points[j].coordinates[0] - point_x <= distance:
+            if j not in processed:
+                if point.distance_to(points[j]) <= distance:
+                    yield j
+            j += 1
+
+        j = i - 1
+        while j > j_min and point_x - points[j].coordinates[0] <= distance:
+            if j not in processed:
+                if point.distance_to(points[j]) <= distance:
+                    yield j
+            j -= 1
 
     for i in range(n_points):
-        if not processed[i]:
-            processed[i] = True
+        if i not in processed:
+            processed.add(i)
             components_sizes.append(1)
-            component_size(i, i+1, i)
+            stack.append(i)
+            while stack:
+                index = stack.pop()
+                for neighbor in neighbors(index, i):
+                    processed.add(neighbor)
+                    components_sizes[-1] += 1
+                    stack.append(neighbor)
+
     components_sizes.sort(reverse=True)
     print(components_sizes)
 
