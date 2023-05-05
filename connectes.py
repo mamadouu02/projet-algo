2567c23d583bb0a551cc3214d9017bcaab1da997
Hamza <hamza.bouihi@grenoble-inp.org>
2023-05-05 15:47:51 +0200

    try_test_grid_algo

--- connectes.py
+++ connectes.py
@@ -22,50 +22,56 @@ def load_instance(filename):
 
     return distance, points
 
+def explore_connexe_comp(dist,points,visited,grid,first,square_neighbor):
+    """Calcule la taille d'une composante connexe"""
+    size,pile = 0,set([first])
+    # Tant que la pile des voisins du sommet traité est non nul faire:
+    while len(pile) != 0 :
+        indice_pts = pile.pop()
+        current_pts = points[indice_pts]
+        approx_pts = int(current_pts.coordinates[0]/dist),int(current_pts.coordinates[1]/dist)
+        
+        # Rend le point courrent traité
+        visited[indice_pts] = True
+        grid[approx_pts].pop(indice_pts)
+        size += 1
 
-def print_components_sizes(distance, points):
+        # Recherche optimisée de possibles vosins 
+        for coord in square_neighbor:
+            key = (approx_pts[0] + coord[0],approx_pts[1] + coord[1])
+            for index_possible_neighbor in grid.get(key,{}):
+                if not visited[index_possible_neighbor]:
+                    if points[indice_pts].distance_to(grid[key][index_possible_neighbor]) <= dist :
+                        pile.add(index_possible_neighbor)
+
+    return size
+
+def print_components_sizes(dist, points):
     """
     affichage des tailles triees de chaque composante
     """
-    n_points = len(points)
-    points.sort()
-    components_sizes = []
-    processed = set()
-    stack = []
+    n_pts,components_sizes,grid = len(points),[],{}
+    visited = [False for _ in range(n_pts)]
+    square_neighbor = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
-    def neighbors(i, j_min):
-        point = points[i]
-        point_x = point.coordinates[0]
+    # On ordonne les points par paquets qui appartiennent au même carré de coté dist 
+    # grâce à l'approximation à l'entier le plus proche + structure de dictionnaire
+    for k in range(n_pts):
+        pts_k = points[k]
+        approx_pts = int(pts_k.coordinates[0]/dist),int(pts_k.coordinates[1]/dist)
+        square = grid.get(approx_pts, {})
+        square[k] = pts_k
+        grid[approx_pts] = square
 
-        j = i + 1
-        while j < n_points and points[j].coordinates[0] - point_x <= distance:
-            if j not in processed:
-                if point.distance_to(points[j]) <= distance:
-                    yield j
-            j += 1
 
-        j = i - 1
-        while j > j_min and point_x - points[j].coordinates[0] <= distance:
-            if j not in processed:
-                if point.distance_to(points[j]) <= distance:
-                    yield j
-            j -= 1
-
-    for i in range(n_points):
-        if i not in processed:
-            processed.add(i)
-            components_sizes.append(1)
-            stack.append(i)
-            while stack:
-                index = stack.pop()
-                for neighbor in neighbors(index, i):
-                    processed.add(neighbor)
-                    components_sizes[-1] += 1
-                    stack.append(neighbor)
+    # Tant que les sommets n'ont pas tous été traités, appliquer la fonction
+    for k in range(n_pts):
+        if not(visited[k]):
+            size = explore_connexe_comp(dist,points,visited,grid,k,square_neighbor)
+            components_sizes += [size]
 
     components_sizes.sort(reverse=True)
     print(components_sizes)
-
 
 def main():
     """
