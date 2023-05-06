def4197bd03520b9dca63288d5bd1b163a0139f3
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-05-07 01:31:02 +0200

    connectes (tree)

--- connectes.py
+++ connectes.py
@@ -7,12 +7,66 @@ sort and display.
 from timeit import timeit
 from sys import argv, setrecursionlimit
 
-from geo.point import Point
+class Point:
+    def __init__(self, coordinates, processed=False):
+        """
+        build new point using an array of coordinates.
+        """
+        self.coordinates = coordinates
+        self.processed = processed
+
+    def distance_to(self, other):
+        """
+        euclidean distance between two points.
+        """
+        if self < other:
+            return other.distance_to(self)  # we are now a symmetric function
+
+        total = 0
+        for c_1, c_2 in zip(self.coordinates, other.coordinates):
+            diff = c_1 - c_2
+            total += diff * diff
+
+        return total**(1/2)
+
+    def __lt__(self, other):
+        """
+        lexicographical comparison
+        """
+        return self.coordinates < other.coordinates
+
+
+def partition(points, axis, g, d):
+    pivot = points[g]
+    m = g
+    for i in range(g+1, d):
+        if points[i].coordinates[axis] < pivot.coordinates[axis]:
+            m += 1
+            if i > m:
+                points[i], points[m] = points[m], points[i]
+    if m > g:
+        points[m], points[g] = points[g], points[m]
+    return m
+
+
+def mediane(points, axis):
+    k = len(points) // 2
+
+    def aux(g, d):
+        m = partition(points, axis, g, d)
+        if m == k:
+            return points[m]
+        elif m < k:
+            return aux(m+1, d)
+        else:
+            return aux(g, m)
+
+    return aux(0, len(points))
 
 
 class Node:
-    def __init__(self, index, axis, left=None, right=None):
-        self.index = index
+    def __init__(self, point, axis, left=None, right=None):
+        self.point = point
         self.axis = axis
         self.left = left
         self.right = right
@@ -20,20 +74,17 @@ class Node:
 
 class Tree:
     def __init__(self, points):
-        indexes = list(range(len(points)))
-        self.points = points
-        self.root = self.construct(indexes)
+        self.root = self.construct(points)
 
-    def construct(self, indexes, depth=0):
-        if not indexes:
+    def construct(self, points, depth=0):
+        if not points:
             return None
 
         axis = depth % 2
-        indexes.sort(key=lambda i : self.points[i].coordinates[axis])
-        m = len(indexes) // 2
-        median = indexes[m]
+        median = mediane(points, axis)
+        m = len(points) // 2
 
-        return Node(median, axis, self.construct(indexes[:m], depth+1), self.construct(indexes[m+1:], depth+1))
+        return Node(median, axis, self.construct(points[:m], depth+1), self.construct(points[m+1:], depth+1))
 
 
 def load_instance(filename):
@@ -55,32 +106,29 @@ def print_components_sizes(distance, points):
     """
     tree = Tree(points)
     components_sizes = []
-    n_points = len(points)
-    processed = [False] * n_points
 
-    def search(node, i):
+    def search(node, point):
         if node is not None:
-            j = node.index
+            other = node.point
             axis = node.axis
-            point, other = points[i], points[j]
             delta = other.coordinates[axis] - point.coordinates[axis]
             if abs(delta) <= distance:
-                if not processed[j] and point.distance_to(other) <= distance:
-                    processed[j] = True
+                if not other.processed and point.distance_to(other) <= distance:
+                    other.processed = True
                     components_sizes[-1] += 1
-                    search(tree.root, j)
-                search(node.left, i)
-                search(node.right, i)
+                    search(tree.root, other)
+                search(node.left, point)
+                search(node.right, point)
             elif delta > 0:
-                search(node.left, i)
+                search(node.left, point)
             else:
-                search(node.right, i)
+                search(node.right, point)
 
-    for i in range(n_points):
-        if not processed[i]:
-            processed[i] = True
+    for point in points:
+        if not point.processed:
+            point.processed = True
             components_sizes.append(1)
-            search(tree.root, i)
+            search(tree.root, point)
 
     components_sizes.sort(reverse=True)
     print(components_sizes)
